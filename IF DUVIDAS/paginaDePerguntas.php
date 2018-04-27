7ac35f8ba64ce7d88a3d02488c19529e763ee86d
juliarenataef <juliarenataifc@gmail.com>
2018-04-27 13:34:00 -0300

    Add files via upload

---
+++ IF DUVIDAS/paginaDePerguntas.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include'head.php' ?>
+
+</head>
+<body>
+
+	<?php include'menu.php' ?>
+
+	<div class="ui sixteen column divided grid">
+		<div class="one wide column"></div>
+		<div class="three wide column">
+			<div class="ui secondary vertical fluid pointing menu">
+				<h3  align="center">Filtrar por</h3>
+				<a class="active item">
+					Mais curtidas 
+				</a>
+				<a class="item">
+					Respondidas
+				</a>
+				<a class="item">
+					Mais comentadas
+				</a>
+			</div>
+		</div>
+
+
+
+
+		<div class="eleven wide column">
+			<h1 align="center" class="ui header">Quanto é 2 mais 2?</h1>
+			<a class="ui green right ribbon label">Respondida</a>
+			<div class="ui vertical segment">
+				<a href="pergunta.php" style="color: inherit; "> <h3 >PERGUNTA</h3></a>
+				<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam 
+					egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+				</div>
+				<div class="ui vertical segment">
+					<h3>PERGUNTA</h3>
+					<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam 
+						egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+				</div>
+				<div class="ui vertical segment">
+						<h3>PERGUNTA</h3>
+						<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+				</div>
+				<div class="ui vertical segment">
+							<h3>PERGUNTA</h3>
+							<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+				</div>
+				<div class="ui vertical segment">
+								<h3>PERGUNTA</h3>
+								<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+				</div>
+				<div class="ui vertical segment">
+									<h3>PERGUNTA</h3>
+									<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+				</div>
+
+								<div class="one wide column"></div>
+
+
+								</div>
+							</div>
+
+							<footer>
+					 			 <?php include'footer.php' ?>
+							</footer>
+
+
+
+
+
+						</body>
+						</html>
